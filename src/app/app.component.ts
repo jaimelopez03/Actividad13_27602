f2585bf4be1726cd0af33f3b06f33cf70d29d383
jaimelopez03 <60837707+jaimelopez03@users.noreply.github.com>
2020-02-23 23:48:42 -0800

    Pryecto 1.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { textareaAutoResize } from 'materialize-css';
+declare let $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'Actividad-Notes';
+export class AppComponent implements OnInit{
+
+  notes = [];
+  constructor() {
+  }
+
+  ngOnInit() {
+    $('.collapsible').collapsible();
+  }
+
+  createnotes(titleInput: HTMLInputElement, textInput: HTMLInputElement) {
+    const titulo = titleInput.value;
+    const texto = textInput.value;
+    if (titulo === '') {
+      alert('No puede haber campos vacios');
+    } else if (texto === '') {
+      alert('No puede haber campos vacios');
+    } else {
+      const newnotes = {
+        title: titulo,
+        text: texto
+      };
+      this.notes.unshift(newnotes);
+      $('#title').val('');
+      $('#textarea').val('');
+      textareaAutoResize($('#textarea'));
+    }
+  }
 }
